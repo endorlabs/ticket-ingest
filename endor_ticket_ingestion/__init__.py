d85b89fe7494c2c10cf48e6c155833ef94d49791
Darren P Meyer <darrenpmeyer@users.noreply.github.com>
2023-03-06 18:02:47 -0600

    Better reporting of Jira HTTP errors

--- endor_ticket_ingestion/__init__.py
+++ endor_ticket_ingestion/__init__.py
@@ -35,17 +35,27 @@ def ingest_findings(secrets, findings_filter, dry_run=False):
         }
 
         # find out if theres already a ticket for this finding
-        jira_results = jira.jql(f'project="{secrets["jira"]["project"]}" and summary ~ "endor: {finding["uuid"]}"')
-        if jira_results['issues']:
-            for issue in jira_results['issues']:
-                print(f'Found matching issue {issue["key"]} {issue["fields"]["summary"]}')  
-        elif dry_run:
-            print('Would create a ticket: '+jira_ticket["summary"])
-        else:
-            # there wasn't a duplicate, let's create a new issue!
-            jira_issue = jira.create_issue(jira_ticket)
-            attachment = io.StringIO(json.dumps(finding["spec"]["reachable_paths"]))
-            attachment.filename = f'reachable_paths-{finding["uuid"]}.json'
-            print(f'Created {jira_issue["key"]}', end='')
-            jira.add_attachment_object(jira_issue["key"], attachment)
+        try:
+            jira_results = jira.jql(f'project="{secrets["jira"]["project"]}" and summary ~ "endor: {finding["uuid"]}"')
+            if jira_results['issues']:
+                for issue in jira_results['issues']:
+                    print(f'Found matching issue {issue["key"]} {issue["fields"]["summary"]}')  
+            elif dry_run:
+                print('Would create a ticket: '+jira_ticket["summary"])
+            else:
+                # there wasn't a duplicate, let's create a new issue!
+                jira_issue = jira.create_issue(jira_ticket)
+                attachment = io.StringIO(json.dumps(finding["spec"]["reachable_paths"]))
+                attachment.filename = f'reachable_paths-{finding["uuid"]}.json'
+                print(f'Created {jira_issue["key"]}', end='')
+                jira.add_attachment_object(jira_issue["key"], attachment)
+        except HTTPError as err:
+            msg = f'HTTP Error communicating with Jira: {err.response.status_code}\n--> {err.response.url}\n--> {err.response.text}'
+            if err.response.status_code == 404:
+                raise RuntimeError(f'{msg}\nPlease check your configuration and make sure your Jira URL is set correctly')
+
+            if err.response.status_code >= 400 and err.response.status_code <= 403:
+                raise RuntimeError(f'{msg}\nPlease check that your userid and token are correct and your account can access the above URL')
+            
+            raise RuntimeError(f'{msg}')
 
